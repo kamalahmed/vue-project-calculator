e8e81fa33cd910bcd0d591428044cc5bfe4f42e5
Kamal Ahmed <kamalacca@gmail.com>
2018-10-12 19:20:42 +0600

    cleaned up iniat boilerplate

---
+++ src/components/Calculator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
