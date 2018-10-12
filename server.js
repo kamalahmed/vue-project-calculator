e50d38addc4fef70e26fb2e3ead58e0698a1e196
Kamal Ahmed <kamalacca@gmail.com>
2018-10-12 23:19:10 +0600

    fixed a typo related bug

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const serverStatic = require('server-static');
+const serveStatic = require('serve-static');
 const path  = require('path');
 
 const app = express();
 // serve app from the dist directory
-app.use('/', serverStatic(path.join(__dirname, '/dist')));
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8080;
 app.listen(port);
